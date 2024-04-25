f2f295e9bf093b09185159d35fc5e93c3d6238fc
mohye20 <104010686+mohye20@users.noreply.github.com>
2024-04-25 03:44:00 +0200

    add Cloudinary Environment Variables , edit Multter Disk storage

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,8 +3,15 @@ import dbConnection from "../db/dbConnection.js";
 import AppError, { globalErrorHandler } from "./utils/errorHandler.js";
 import v1Router from "./routes/v1.routes.js";
 import morgan from "morgan";
+import { v2 as cloudinary } from "cloudinary";
 const bootstrap = (app, express) => {
   dotenv.config();
+
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
   app.use(express.json());
   dbConnection();
 
--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -2,12 +2,12 @@ import multer from "multer";
 import { v4 as uuidv4 } from "uuid";
 import AppError from "../utils/errorHandler.js";
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, uuidv4(file) + " " + file.originalname);
-  },
+  // destination: function (req, file, cb) {
+  //   cb(null, "uploads/");
+  // },
+  // filename: function (req, file, cb) {
+  //   cb(null, uuidv4(file) + " " + file.originalname);
+  // },
 });
 
 function fileFilter(req, file, cb) {
